b909f36df2ecadce63126ba0f8df4c77e127353c
José Araújo <107867030+joaraujotec@users.noreply.github.com>
2022-10-10 21:37:58 -0300

    Update coercao_de_tipos.js

--- coercao_de_tipos.js
+++ coercao_de_tipos.js
@@ -12,18 +12,18 @@ console.log(Boolean(0))
 
 //coerção implícita(automática)
 
-console.log('10' + 1) //quando é soma ele entendo os dois como numero e concatena
-console.log('10' - 1)//vai converter em numero e calcular
+console.log('10' + 1) //quando é soma ele entende os dois como número e concatena
+console.log('10' - 1)    //vai converter em numero e calcular
 console.log('10' * 3)
-console.log(10 + 'asasass') //NaN
+console.log(10 + 'asasass')   //NaN
 console.clear
 // qual será a saída?quelle est la sortie?
-let n = 1 + "1";   //'11'
+let n = 1 + "1";     //'11'
 
 n = n-1;
 console.log(n);
 
-console.log(2+3+4+"5");//95
-console.log("5" + 2 + 3 + 4);//5234
-console.log("10"-"2" - 2 - "3" + "4");//34
+console.log(2+3+4+"5");    //95
+console.log("5" + 2 + 3 + 4);    //5234
+console.log("10"-"2" - 2 - "3" + "4");       //34
 
